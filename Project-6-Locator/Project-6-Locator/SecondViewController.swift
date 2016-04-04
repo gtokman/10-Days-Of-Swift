09bbcfae10a8347905d0033f03019c316bc41643
gtokman <gtokman@fullsail.edu>
2016-04-03 21:46:17 -0400

    Project 6 Initial Commit

---
+++ Project-6-Locator/Project-6-Locator/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Project-6-Locator
+//
+//  Created by g tokman on 4/3/16.
+//  Copyright © 2016 garytokman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
