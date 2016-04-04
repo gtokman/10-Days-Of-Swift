71f17ce0bc0d360cd01c9dd3b7da87dd03974819
gtokman <gtokman@fullsail.edu>
2016-04-03 22:19:32 -0400

    Setup of the locator UI

--- Project-6-Locator/Project-6-Locator/FirstViewController.swift
+++ Project-6-Locator/Project-6-Locator/FirstViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import CoreLocation
 
-class FirstViewController: UIViewController {
+class FirstViewController: UITableViewController {
+    
+    // MARK: Properties
+    
+    var locationManager = CLLocationManager()
+    var currentLocation = CLLocation()
 
+    // MARK: Life Cycle
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        askLocationPermission()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    // MARK: Actions
+
+    @IBAction func addLocationButton(sender: UIBarButtonItem) {
+        
     }
-
-
 }
 
+extension FirstViewController {
+    func askLocationPermission() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        
+        switch (CLLocationManager.authorizationStatus()) {
+       
+        case .AuthorizedWhenInUse, .AuthorizedAlways:
+            locationManager.stopUpdatingLocation()
+        case .Denied:
+            let alert = UIAlertController(title: "Permissions error", message: "This app needs location permission to find you!", preferredStyle: .Alert)
+            let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            alert.addAction(okAction)
+            presentViewController(alert, animated: true, completion: nil)
+        case .NotDetermined:
+            fallthrough
+        default:
+            locationManager.requestWhenInUseAuthorization()
+    }
+}
--- Project-6-Locator/Project-6-Locator/SecondViewController.swift
+++ Project-6-Locator/Project-6-Locator/SecondViewController.swift
@@ -7,17 +7,18 @@
 //
 
 import UIKit
+import MapKit
 
 class SecondViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    @IBOutlet var mapView: MKMapView?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
