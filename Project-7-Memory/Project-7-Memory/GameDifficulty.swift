690de83e429f6737190dd13d6c5e50669036d906
gtokman <gtokman@fullsail.edu>
2016-04-04 23:33:10 -0400

    Add difficulty game buttons

---
+++ Project-7-Memory/Project-7-Memory/GameDifficulty.swift
@@ -0,0 +1,15 @@
+//
+//  GameDifficulty.swift
+//  Project-7-Memory
+//
+//  Created by g tokman on 4/4/16.
+//  Copyright © 2016 garytokman. All rights reserved.
+//
+
+import Foundation
+
+enum Difficulty {
+    case Easy
+    case Medium
+    case Hard
+}
--- Project-7-Memory/Project-7-Memory/ViewController.swift
+++ Project-7-Memory/Project-7-Memory/ViewController.swift
@@ -9,17 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setup()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+// MARK: Setup
+
+private extension ViewController {
+    func setup() {
+        view.backgroundColor = .whiteColor()
+        
+        // Add buttons
+        buildButtonWithCenter(CGPoint(x: view.center.x, y: view.center.y / 2.0), title: "Easy", color: .greenColor(), action: #selector(ViewController.onEasyTapped(_:)))
+        buildButtonWithCenter(CGPoint(x: view.center.x, y: view.center.y), title: "Meduim", color: .yellowColor(), action: #selector(ViewController.onMediumTapped(_:)))
+        buildButtonWithCenter(CGPoint(x: view.center.x, y: view.center.y  * 3.0 / 2.0), title: "Hard", color: .redColor(), action: #selector(ViewController.onHardTapped(_:)))
+    }
+    
+    func buildButtonWithCenter(center: CGPoint, title: String, color: UIColor, action: Selector) {
+        let button = UIButton()
+        button.setTitle(title, forState: .Normal)
+        button.setTitleColor(.blackColor(), forState: .Normal)
+        
+        button.frame = CGRect(origin: CGPoint(x: 0, y:  0), size: CGSize(width: 200, height: 50))
+        button.center = center
+        button.backgroundColor = color
+        
+        button.addTarget(self, action: action, forControlEvents: .TouchUpInside)
+        
+        view.addSubview(button)
+    }
+}
+
+// MARK: Difficulty
+
+extension ViewController {
+    func onEasyTapped(sender: UIButton) {
+        newGameDifficulty(.Easy)
+    }
+    
+    func onMediumTapped(sender: UIButton) {
+        newGameDifficulty(.Medium)
+    }
+    
+    func onHardTapped(sender: UIButton) {
+        newGameDifficulty(.Hard)
+    }
+    
+    func newGameDifficulty(difficulty: Difficulty) {
+        
+        switch difficulty {
+        case .Easy:
+            print("Easy")
+        case .Medium:
+            print("Medium")
+        case .Hard:
+            print("Hard")
+        }
+    }
+}
