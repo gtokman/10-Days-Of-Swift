6672625bad9675db1f7e193386acb3a8e935eb9a
gtokman <gtokman@fullsail.edu>
2016-04-05 12:06:28 -0400

    Create Card Deck Data Model

---
+++ Project-7-Memory/Project-7-Memory/CardCell.swift
@@ -0,0 +1,13 @@
+//
+//  CardCellCollectionViewCell.swift
+//  Project-7-Memory
+//
+//  Created by g tokman on 4/5/16.
+//  Copyright © 2016 garytokman. All rights reserved.
+//
+
+import UIKit
+
+class CardCellCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Project-7-Memory/Project-7-Memory/Deck.swift
@@ -0,0 +1,9 @@
+//
+//  Deck.swift
+//  Project-7-Memory
+//
+//  Created by g tokman on 4/5/16.
+//  Copyright © 2016 garytokman. All rights reserved.
+//
+
+import Foundation
