fea9304ae9fff70ae9f3f9dff1ed011b9d3af6cb
gtokman <gtokman@fullsail.edu>
2016-04-04 21:41:06 -0400

    Project 7 Initial Commit

---
+++ Project-7-SnapChatMenu/Project-7-SnapChatMenu/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project-7-SnapChatMenu
+//
+//  Created by g tokman on 4/4/16.
+//  Copyright © 2016 garytokman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: Outlets
+    
+    @IBOutlet var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
