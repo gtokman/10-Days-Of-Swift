7eb4c8309165d6250f5ec234e2cf5b8c89bc6616
gtokman <gtokman@fullsail.edu>
2016-04-13 13:01:41 -0400

    Added Image picker

---
+++ Project-10-FoodTracker/Project-10-FoodTracker/RatingController.swift
@@ -0,0 +1,13 @@
+//
+//  RatingController.swift
+//  Project-10-FoodTracker
+//
+//  Created by g tokman on 4/13/16.
+//  Copyright © 2016 garytokman. All rights reserved.
+//
+
+import UIKit
+
+class RatingController: UIView {
+    // MARK: - Initialization
+}
