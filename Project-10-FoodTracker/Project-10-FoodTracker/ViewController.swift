20c59ecfd0f4b5b99618ee3effa3a4fa02d01051
gtokman <gtokman@fullsail.edu>
2016-04-13 11:43:29 -0400

    Project 10 Intitial Commit

---
+++ Project-10-FoodTracker/Project-10-FoodTracker/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Project-10-FoodTracker
+//
+//  Created by g tokman on 4/13/16.
+//  Copyright © 2016 garytokman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+}
