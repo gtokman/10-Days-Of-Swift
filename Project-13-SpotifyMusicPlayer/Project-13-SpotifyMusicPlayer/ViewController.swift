91680e2646679296175dfa292ea155fc87861bbc
gtokman <gtokman@fullsail.edu>
2016-04-16 21:11:31 -0400

    Project 13 Initial Commit

---
+++ Project-13-SpotifyMusicPlayer/Project-13-SpotifyMusicPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project-13-SpotifyMusicPlayer
+//
+//  Created by g tokman on 4/16/16.
+//  Copyright © 2016 garytokman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
